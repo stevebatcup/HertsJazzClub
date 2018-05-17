785caf905ea3b8c31ad3e586fdd821c1f782aae3
Steven Batcup <steve.batcup@gmail.com>
2018-05-17 13:16:24 +0100

    last fix?

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -7,7 +7,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/hertsjazz/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="rbenv exec bundle exec $APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT && bundle exec $APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
 set -u
 
