bc46882b17e4a9eb3ef0e097c41fb94fef76daeb
Steven Batcup <steve.batcup@gmail.com>
2018-05-17 12:55:36 +0100

    last fix?

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/var/www/hertsjazz"
-pid "/var/www/hertsjazz/tmp/pids/unicorn.pid"
-stderr_path "/var/www/hertsjazz/log/unicorn.log"
-stdout_path "/var/www/hertsjazz/log/unicorn.log"
+working_directory "/var/www/hertsjazz/current/"
+pid "/var/www/hertsjazz/current/tmp/pids/unicorn.pid"
+stderr_path "/var/www/hertsjazz/current/log/unicorn.log"
+stdout_path "/var/www/hertsjazz/current/log/unicorn.log"
 
 listen "/tmp/hertsjazz_unicorn.sock"
 worker_processes 2
