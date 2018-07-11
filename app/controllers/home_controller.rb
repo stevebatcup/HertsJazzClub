d06518af19c8e5a9288e2f9f09cb6ffd46ed0435
Steven Batcup <steve.batcup@gmail.com>
2018-07-11 13:42:54 +0100

    deal with no gigs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		@upcoming_gigs = nil
+		@upcoming_gigs = []
 		if @next_gig = Gig.next
 			@upcoming_gigs = Gig.upcoming(3, "'#{@next_gig.ends.strftime("%Y-%m-%d")}'")
 		end
