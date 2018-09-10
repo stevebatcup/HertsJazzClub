70b116e78dcdd2170985708f8586ed564c8751f8
Steven Batcup <steve.batcup@gmail.com>
2018-09-10 06:58:53 +0100

    show on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 		@content = PageContent.find_by(page: "home")
 		@upcoming_gigs = []
 		if @next_gig = Gig.next
-			@upcoming_gigs = Gig.upcoming(3, "'#{@next_gig.ends.strftime("%Y-%m-%d")}'")
+			@upcoming_gigs = Gig.upcoming(3, "'#{@next_gig.starts.strftime("%Y-%m-%d")}'")
 		end
 	end
 end
